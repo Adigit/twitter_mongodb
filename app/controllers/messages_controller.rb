fc415653d091f00d5dd71481a654582eed29c371
Adigit <adi.anu.ducs@gmail.com>
2014-11-19 19:28:27 +0000

    config changed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,11 +4,11 @@ class MessagesController < ApplicationController
   def create
     @conversation = Conversation.find(params[:conversation_id])
     logger.error ">>>> @conversation >>>> #{@conversation.inspect}"
-    logger.error ">>>> @conversation.message >>>> #{@conversation.messages.inspect}"
+    
     @message = @conversation.messages.build(message_params)
     @message.user_id = current_user.id
     @message.save!
- 
+    logger.error ">>>> @conversation.message >>>> #{@conversation.messages.inspect}"
     @path = conversation_path(@conversation)
   end
  
